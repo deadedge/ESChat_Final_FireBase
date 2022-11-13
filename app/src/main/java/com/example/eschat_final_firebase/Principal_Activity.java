7b8cd580ea44212f1d716431f65fcc20d7dcbde0
João Correia <a2021011@alunos.sefo.pt>
2022-11-13 21:33:36 +0000

    Initial Commit

--- app/src/main/java/com/example/eschat_final_firebase/Principal_Activity.java
+++ app/src/main/java/com/example/eschat_final_firebase/Principal_Activity.java
@@ -1,6 +1,9 @@
 package com.example.eschat_final_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -16,5 +19,44 @@ public class Principal_Activity extends AppCompatActivity {
         binding=ActivityPrincipalBinding.inflate(getLayoutInflater());
         getSupportActionBar().hide();
         setContentView(binding.getRoot());
+        replaceFragment(new MensagensFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->
+        {
+
+            switch (item.getItemId())
+            {
+                case R.id.mensagens:
+                    replaceFragment(new MensagensFragment());
+                    break;
+                case R.id.pesquisa:
+                    replaceFragment(new ProcurarFragment());
+
+                    break;
+                case R.id.foto:
+                    replaceFragment(new AdicionarFotoFragment());
+
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+
+                    break;
+
+
+            }
+
+
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
