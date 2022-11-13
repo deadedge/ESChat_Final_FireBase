f8b80b7835c5be0447d271a50a2c3466cb2c7d25
João Correia <a2021011@alunos.sefo.pt>
2022-11-13 15:06:07 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/eschat_final_firebase/Continuar_Registro_Activity.java
@@ -0,0 +1,14 @@
+package com.example.eschat_final_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Continuar_Registro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continuar_registro);
+    }
+}
