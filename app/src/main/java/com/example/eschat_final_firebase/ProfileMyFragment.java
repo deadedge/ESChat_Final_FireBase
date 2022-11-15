d05aa897320d0ef4f5216ce363bd5ee8ae1442f3
João Correia <a2021011@alunos.sefo.pt>
2022-11-15 11:32:35 +0000

    Initial Commit

--- app/src/main/java/com/example/eschat_final_firebase/ProfileMyFragment.java
+++ app/src/main/java/com/example/eschat_final_firebase/ProfileMyFragment.java
@@ -2,11 +2,15 @@ package com.example.eschat_final_firebase;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.eschat_final_firebase.databinding.FragmentMyProfileBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +19,7 @@ import android.view.ViewGroup;
  */
 public class ProfileMyFragment extends Fragment {
 
+    private FragmentMyProfileBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -54,14 +59,19 @@ public class ProfileMyFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+        binding=FragmentMyProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Todo o codigo do fragmento vai ficar aquo
+
     }
 }
